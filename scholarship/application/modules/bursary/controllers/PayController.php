56faf716a002a2ea39fddd9b9a72d6ddd7e20585
liuyanwen <yeawin@xmu.edu.cn>
2016-09-22 20:42:20 +0800

    查看抵扣清单

--- scholarship/application/modules/bursary/controllers/PayController.php
+++ scholarship/application/modules/bursary/controllers/PayController.php
@@ -55,7 +55,7 @@ class Bursary_PayController extends Zend_Controller_Action
 //         echo ($table);exit();
         $this->view->table = $table;
     }
-    
+
     protected function grant_detail($apply)
     {
         date_default_timezone_set("PRC");
@@ -143,18 +143,38 @@ class Bursary_PayController extends Zend_Controller_Action
         }
         return $table;
     }
-    
-    private function udate($format = 'u', $utimestamp = null) {
+
+    private function udate($format = 'u', $utimestamp = null)
+    {
         if (is_null($utimestamp))
             $utimestamp = microtime(true);
     
             $timestamp = floor($utimestamp);
             $milliseconds = round(($utimestamp - $timestamp) * 1000000);
     
-            return date(preg_replace('`(?<!\\\\)u`', $milliseconds, $format), $timestamp);
+            return date(preg_replace('`(?<!\\)u`', $milliseconds, $format), $timestamp);
     }
 
+    public function printAction()
+    {
+        //奖学金信息
+        $scholarship_id = $this->getParam("id");
+        $Scholarship = new Application_Model_Bursaryinfo();
+        $scholarship = $Scholarship->get_scholarship_record($scholarship_id);
+        $this->view->scholarship = $scholarship;
+        
+        //申请人
+        $Apply = new Application_Model_Bursaryapply();
+        $where_array = array("a.is_paid = '1'", "a.is_pass = '1'", "a.scholarship_id='$scholarship_id'");
+        $order_array = array("a.scholarship_id", "a.apply_time");
+        $apply_list = $Apply->get_apply_list($where_array, $order_array);
+        $this->view->apply_list = $apply_list;
+    }
+
+
 }
+
+
 
 
 
--- scholarship/application/modules/tuition/controllers/ChargingController.php
+++ scholarship/application/modules/tuition/controllers/ChargingController.php
@@ -18,15 +18,18 @@ class Tuition_ChargingController extends Zend_Controller_Action
         // action body
         $Params = $this->getAllParams();
         $where_array = null;
-        if (isset($Params["id"])) {
-            $where_array = array(
-                "s.stu_id = '" . $Params["id"] . "'"
-            );
+        $i = 0;
+        if (isset($Params["stu_id"])) {
+            $where_array[$i ++] = "s.stu_id = '" . $Params["stu_id"] . "'";
+        }
+        if (isset($Params["scholarship_id"])) {
+            $where_array[$i ++] = "h.scholarship_id = '" . $Params["scholarship_id"] . "'";
         }
         $order_array = array(
             "s.stu_id asc",
             "h.tuition_key desc",
         );
+//         var_dump($where_array);return false;
         $History = new Application_Model_Tuitionhistory();
         $history_list = $History->get_history_list($where_array, $order_array);
         $this->view->history_list = $history_list;
