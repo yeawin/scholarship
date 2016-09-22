b2e046aab8cdbe911f9d5b455f37dd10839278f4
liuyanwen <yeawin@xmu.edu.cn>
2016-09-22 20:56:20 +0800

    管理员添加

--- scholarship/application/modules/user/controllers/ManagerController.php
+++ scholarship/application/modules/user/controllers/ManagerController.php
@@ -16,6 +16,14 @@ class User_ManagerController extends Zend_Controller_Action
     public function addAction()
     {
         // action body
+        $Dept = new Application_Model_Deptinfo();
+        $parent_code = STUDENT_DEPT;
+        $dept_list = $Dept->get_dept_list($parent_code);
+        $this->view->dept_list = $dept_list;
+        
+        $Usertype = new Application_Model_Usertype();
+        $user_type_list = $Usertype->get_type_list();
+        $this->view->user_type_list = $user_type_list;
     }
 
     public function addOkAction()
